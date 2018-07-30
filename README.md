7c7386681c7ff1e62e263259c2b27bc82d1a958e
WeiJietao <13415156317@163.com>
2018-07-30 18:14:55 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Ammunition Storage #
+
+> 各种公共工具
+
+## JavaScript ##
+
+1.
+
+## Node.js ##
+
+1. fs-copy: 目录/文件拷贝
+
+---
+
+```
+DATE: 2018-07-30
+```
---
+++ src/entry.js
@@ -0,0 +1,6 @@
+var cookieUtil = require('./cookie-util.js');
+var md5 = require('./md5.js');
+
+window.Ammunition = function() {};
+Ammunition.prototype.cookieUtil = cookieUtil;
+Ammunition.prototype.md5 = md5;
