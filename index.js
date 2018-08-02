55f28e96110efef9c5d2c5b24b0f8906e140a060
WeiJietao <13415156317@163.com>
2018-08-02 11:45:16 +0800

    初始化v0.0.2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const copyDir = require('./lib/copy-dir.js');
+const dateFormatInit = require('./../lib/date-format.js');
 
 module.exports = {
+    init: function() {
+        dateFormatInit();
+    },
     copyDir: copyDir,
 }
--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,12 @@
 var cookieUtil = require('./cookie-util.js');
 var md5 = require('./md5.js');
+var dateFormatInit = require('./../lib/date-format.js');
 
 function Ammunition() {};
 Ammunition.prototype = {
+    init: function() {
+        dateFormatInit();
+    },
     cookieUtil: cookieUtil,
     md5: md5
 };
