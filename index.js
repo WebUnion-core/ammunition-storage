8dc1e07255e580d5455f55f820ead248d45e9af4
WeiJietao <13415156317@163.com>
2018-08-19 23:12:09 +0800

    更新0.0.3版本

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const copyDir = require('./lib/copy-dir.js');
 const dateFormatInit = require('./lib/date-format.js');
 const md5 = require('./lib/md5.js');
+const cookie = require('./lib/cookie-util.js');
 
 module.exports = {
     init: function() {
@@ -8,4 +9,5 @@ module.exports = {
     },
     copyDir: copyDir,
     md5: md5,
+    cookie: cookie
 }
